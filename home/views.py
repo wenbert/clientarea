7ff5ce4062e85931c9a3ae1e8ceb506c57fce3ff
wenbert <wenbert@ubuntu-wenbert.(none)>
2010-11-08 23:02:13 +0100

    added breadcrumbs. fix weird behavior. try to navigate. it doesn't feel right. too sleepy.

--- home/views.py
+++ home/views.py
@@ -19,6 +19,8 @@ def browse_files(request,groupname):
     data = {}
     user = request.user
     
+    url_dir = request.GET.get('dir')
+    
     path = settings.APPLICATION_STORAGE
 
     files = []
@@ -30,36 +32,50 @@ def browse_files(request,groupname):
          return render_to_response('404.html')
     else:
         pass
-    
+
     grouppath = os.path.join(path, str(groupname)) #join group
     
     if request.GET.get('dir'):
-        grouppath = os.path.join(grouppath,str(request.GET.get('dir')))
+        grouppath = os.path.join(grouppath,str(url_dir))
         if settings.APPLICATION_STORAGE not in grouppath: #make sure that grouppath is inside the APPLICATION_STORAGE
             return render_to_response('404.html')
         elif "../" in grouppath: #make sure not to allow relative paths, etc.
             return render_to_response('404.html')
-    
+
     if os.path.exists(str(grouppath)):
         contents = os.listdir(str(grouppath)) #contents of the current directory
         for i in contents:
             if os.path.isfile(os.path.join(grouppath, i)):
                 if request.GET.get('dir'):
-                    files.append({'name':i,'path':os.path.join(request.GET.get('dir'),i),'group':groupname})
+                    files.append({'name':i,'path':os.path.join(url_dir,i),'group':groupname})
                 else:
                     files.append({'name':i,'path':i,'group':groupname})
             if os.path.isdir(os.path.join(grouppath, i)):
                 if request.GET.get('dir'):
-                    directories.append({'name':i,'path':os.path.join(request.GET.get('dir'),i),'group':groupname})
+                    directories.append({'name':i,'path':os.path.join(url_dir,i),'group':groupname})
                 else:
                     directories.append({'name':i,'path':i,'group':groupname})
-        
+    
+    """
+    Split URL pieces using the "os.path.seperator-thingy"
+    There should be something like that...
+    """
+    url_pieces = str(url_dir).split(os.sep)
+    crumbs = []
+    for i in range(len(url_pieces)):
+        #url_pieces[i] += str(url_pieces[i-1]),str(os.pathsep),str(url_pieces[i])
+        crumbs.append({'name':url_pieces[i],'path':str(url_pieces[0:i]).replace(" ","").replace("[","").replace("]","").replace("'","").replace(",","/")})
+        #crumbs.append({'path': url_pieces[0:i]})
+    
     data = {
         "location": path,
+        "url_dir": url_dir,
         "files": files,
         "directories": directories,
         "grouppath": grouppath,
         "groupname": groupname,
+        "url_pieces": url_pieces,
+        "crumbs": crumbs,
     }
     
     return render_to_response("home/browse_files.html",
